89952ca252c1d02dc4cd2091458b00dd5c9f8862
ankhod <ankhod7@gmail.com>
2025-03-29 23:29:03 +0200

    store.js

---
+++ src/redux/store.js
@@ -0,0 +1,42 @@
+import { configureStore } from '@reduxjs/toolkit';
+import contactsReducer from './contactsSlice';
+import filtersReducer from './filtersSlice';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+// Конфігурація для збереження лише поля items у localStorage
+const contactsPersistConfig = {
+  key: 'contacts',
+  storage,
+  whitelist: ['items'], // Зберігаємо тільки items
+};
+
+// Обгортаємо contactsReducer у persistReducer
+const persistedContactsReducer = persistReducer(
+  contactsPersistConfig,
+  contactsReducer
+);
+
+export const store = configureStore({
+  reducer: {
+    contacts: persistedContactsReducer,
+    filters: filtersReducer,
+  },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
